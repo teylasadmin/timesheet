86972f2b557685ce9f36d8c16e20844f62e38b0c
stachu <digitalbrick.office@gmail.com>
2021-02-01 09:13:18 +0100

    logo in footer

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     <div className={styles.footer}>
         <span className={styles.footer_span}>
           Powered by{' '}
-          <img src="/vercel.svg" alt="Teylas Solutions Sp. z o.o." className={styles.footer_logo} />
+          <img src="/teylas_w283.svg" alt="Teylas Solutions Sp. z o.o." className={styles.footer_logo} />
         </span>
     </div>
   );
