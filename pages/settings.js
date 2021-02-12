9e4b3e2519b0d9b96fc24eb08fbc7543a6e498b1
stachu <digitalbrick.office@gmail.com>
2021-02-12 21:05:56 +0100

    testing routing

--- pages/settings.js
+++ pages/settings.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Layout from "../components/Layout";
 
 export default function Settings() {
-
-
-
    return (
-    <form onSubmit="" noValidate>
-              <TextField id="mo"
-                         name="project_name" // find an array of short names of week days
-                         label="Project Name"
-                          InputLabelProps={{
-                            shrink: true,
-                          }}
+   <Layout>
+            <form onSubmit="" noValidate>
+                      <TextField id="mo"
+                                 name="project_name" // find an array of short names of week days
+                                 label="Project Name"
+                                  InputLabelProps={{
+                                    shrink: true,
+                                  }}
 
-                         placeholder="Project Name"
-              />
-
-   </form>
+                                 placeholder="Project Name"
+                      />
+           </form>
+   </Layout>
    )
 }
